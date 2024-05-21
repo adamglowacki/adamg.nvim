47e6452debd7ccf9df879799af6a2872b5c68da6
Adam Głowacki <adam.glowacki@interia.pl>
2024-05-21 14:27:54 +0200

    go back to gruvbox

--- init.lua
+++ init.lua
@@ -189,22 +189,22 @@ require('lazy').setup({
     },
   },
 
-  -- {
-  --   'ellisonleao/gruvbox.nvim',
-  --   priority = 1000,
-  --   config = function()
-  --     vim.cmd.colorscheme 'gruvbox'
-  --   end,
-  -- },
-  
   {
-    'catppuccin/nvim',
-    name = 'catppuccin',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'gruvbox'
     end,
   },
+
+  -- {
+  --   'catppuccin/nvim',
+  --   name = 'catppuccin',
+  --   priority = 1000,
+  --   config = function()
+  --     vim.cmd.colorscheme 'catppuccin'
+  --   end,
+  -- },
 
   {
     -- Set lualine as statusline
